fb6d45d014c1e9b0492cc634151224b41e0eaccc
clima365 <rty.94@mail.ru>
2019-02-21 23:40:15 +0300

    form added

--- src/main.js
+++ src/main.js
@@ -1,1 +1,38 @@
 import "./main.styl";
+
+const form = document.getElementById("form");
+const button = document.getElementById("formButton");
+
+function formSubmitting() {
+  const values = {};
+  const errors = [];
+  const errorsText = document.getElementById("errors");
+  const successText = document.getElementById("success");
+  Object.keys(form).forEach(item => {
+    values[form[item].name] = form[item].value;
+  });
+
+  if (!values.email) {
+    errors.push("Email is required");
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+    errors.push("Invalid email address");
+  }
+
+  if (!values.name) {
+    errors.push("Name is required");
+  }
+
+  if (errors.length > 0) {
+    errorsText.innerHTML = errors.join(", ");
+  } else {
+    errorsText.innerHTML = "";
+    successText.innerHTML = "Thank you!";
+  }
+}
+
+function stopDefAction(e) {
+  e.preventDefault();
+}
+
+form.addEventListener("click", stopDefAction, false);
+button.addEventListener("click", formSubmitting, false);
